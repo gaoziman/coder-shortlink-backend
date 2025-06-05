075ba809002687ecaba0dde4c720cd3fc451cc14
Leo <98382335+gaoziman@users.noreply.github.com>
2025-06-05 21:30:51 +0800

    短链接项目初始化

---
+++ coder-shortlink-admin/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author : 程序员Leo
+ * @date 2025-06-05 20:31
+ * @version 1.0
+ * @description :
+ */
+module coder.shortlink.admin {
+}
---
+++ coder-shortlink-gateway/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author : 程序员Leo
+ * @date  2025-06-05 20:28
+ * @version 1.0
+ * @description :
+ */
+module coder.shortlink.gateway {
+}
