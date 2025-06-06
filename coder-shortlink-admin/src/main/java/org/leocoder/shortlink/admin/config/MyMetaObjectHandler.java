4578b93dc62309af020ddcb1d5b82c5bafad34ad
Leo <98382335+gaoziman@users.noreply.github.com>
2025-06-06 19:29:16 +0800

    feat: 开发短链接后管系统通用持久层对象属性填充

---
+++ coder-shortlink-admin/src/main/java/org/leocoder/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,33 @@
+package org.leocoder.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author : 程序员Leo
+ * @version 1.0
+ * @date 2025-06-06 19:11
+ * @description : 自定义填充公共字段
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("开始插入填充...");
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag",   Integer.class,  0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("开始更新填充...");
+        this.strictUpdateFill(metaObject, "updateTime",Date::new, Date.class);
+    }
+}
